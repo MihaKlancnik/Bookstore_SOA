363128f06827739a9adc3eb47624dfb6589ea5fd
Benjamin Jeran <108634496+Oblakoru@users.noreply.github.com>
2024-12-30 14:09:57 +0100

    Dodan zacetni page za registracijo in prijavo

---
+++ BenjaminJeran/SOA_Bookcase_Frontend/src/routes/app/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	let { children } = $props();
+</script>
+
+<nav class="flex flex-row justify-between bg-indigo-500 p-4">
+	<a href="/">Domov</a>
+	<a href="/app/books">Knjige</a>
+	<a href="/app/cart">Košarica</a>
+	<a href="/app/user">Uporabnik</a>
+</nav>
+
+{@render children()}
+

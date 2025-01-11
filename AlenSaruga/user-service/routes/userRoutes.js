7fa98904f636ac8179d02ff5de0a6a17f7a50125
AlenSaruga <alen.saruga01@gmail.com>
2025-01-11 16:38:07 +0100

    Add Swagger documentation for user login endpoint

--- AlenSaruga/user-service/routes/userRoutes.js
+++ AlenSaruga/user-service/routes/userRoutes.js
@@ -3,6 +3,87 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: User Login
+ *     description: Authenticate a user and return a JSON Web Token (JWT).
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: The user's email address.
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 description: The user's password.
+ *             required:
+ *               - email
+ *               - password
+ *     responses:
+ *       200:
+ *         description: Successful login
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   description: The JWT token returned on successful login.
+ *       400:
+ *         description: Bad request, missing email or password
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message for missing fields.
+ *       401:
+ *         description: Unauthorized, invalid email or password
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message for invalid login credentials.
+ *       404:
+ *         description: User not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message if the user is not found.
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message for server issues.
+ */
+router.get('/login', userController.login);
+
+
 /**
  * @swagger
  * /:

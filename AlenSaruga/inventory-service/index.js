163459bf5419b5e8250d72a235cee15c1ec85988
AlenSaruga <alen.saruga01@gmail.com>
2025-01-11 22:22:34 +0100

    Add JWT authentication middleware to inventory and user services

--- AlenSaruga/inventory-service/index.js
+++ AlenSaruga/inventory-service/index.js
@@ -3,10 +3,28 @@ const inventoryRoutes = require('./routes/inventoryRoutes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 app.use(cors());
 const PORT = 4002;
+
+const verifyJWT = (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1]; 
+
+    if (!token) {
+        return res.status(403).json({ message: 'Token is required' }); 
+    }
+
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        req.user = decoded;
+        next();
+    });
+};
 
 const swaggerOptions = {
     swaggerDefinition: {
@@ -26,11 +44,12 @@ const swaggerOptions = {
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
 
 app.use('/api/inventory', inventoryRoutes);
+app.use('/api/inventory', verifyJWT, inventoryRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(PORT, () => {
     console.log(`Inventory service is running on http://localhost:${PORT}/api/inventory`);

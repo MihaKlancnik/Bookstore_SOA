9bab247403c5249356aedbb759d1e6fb7edb974b
AlenSaruga <alen.saruga01@gmail.com>
2025-01-19 18:03:59 +0100

    Add daily deals service and update GraphQL server connection; refactor user page visit logging and notification fetching

--- BenjaminJeran/SOA_Bookcase_Frontend/src/routes/app/user/+page.js
+++ BenjaminJeran/SOA_Bookcase_Frontend/src/routes/app/user/+page.js
@@ -6,19 +6,19 @@ const headers = {
 
 async function logVisit(page) {
     try {
-      const response = await fetch("http://localhost:5000/log-visit", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ page }),
-      });
-      
+        const response = await fetch("http://localhost:5000/log-visit", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ page }),
+        });
+
     } catch (error) {
-      console.error("Error logging visit:", error);
+        console.error("Error logging visit:", error);
     }
-  }
+}
 
 
-  const fetchNotifications = async () => {
+const fetchNotifications = async () => {
     try {
         const response = await fetch('https://notification-service-v1-0.onrender.com/api/notifications');
 
@@ -61,46 +61,49 @@ export async function load() {
 
             const notificationsResponse = await fetch('https://notification-service-v1-0.onrender.com/api/notifications');
             console.log("notificationsResponse", notificationsResponse);
-  
+
+            let notifications;
+
             if (!notificationsResponse.ok) {
-                throw new Error('Failed to fetch notifications.');
+                notifications = 'Ni novih obvestil.';
+            } else {
+                notifications = await notificationsResponse.text();
             }
-  
-            const notifications = await notificationsResponse.text();
+
 
             return { users, notifications };
         }
-        
+
         if (role === "user") {
-          let userId = decodedPayload.sub;
-          console.log("Prijavlen z uporabniškim računom: " + userId);
-          
-          // Fetching the user information
-          const userResponse = await fetch(`http://localhost:4001/api/users/${userId}`, { headers });
+            let userId = decodedPayload.sub;
+            console.log("Prijavlen z uporabniškim računom: " + userId);
 
-          if (!userResponse.ok) {
-              throw error(500, 'Failed to load user data');
-          }
+            // Fetching the user information
+            const userResponse = await fetch(`http://localhost:4001/api/users/${userId}`, { headers });
 
-          const users = await userResponse.json();
-          console.log("Tukaj je user");
-          console.log(users);
+            if (!userResponse.ok) {
+                throw error(500, 'Failed to load user data');
+            }
 
-          // Fetching the orders for the user
-          const ordersResponse = await fetch(`http://localhost:2001/orders/user/${userId}`, { headers });
+            const users = await userResponse.json();
+            console.log("Tukaj je user");
+            console.log(users);
 
-          if (!ordersResponse.ok) {
-              throw error(500, 'Failed to load orders data');
-          }
+            // Fetching the orders for the user
+            const ordersResponse = await fetch(`http://localhost:2001/orders/user/${userId}`, { headers });
 
-          const orders = await ordersResponse.json();
-          console.log(orders)
-  
-           
-          return { users, orders };
-      }
+            if (!ordersResponse.ok) {
+                throw error(500, 'Failed to load orders data');
+            }
+
+            const orders = await ordersResponse.json();
+            console.log(orders)
+
+
+            return { users, orders };
+        }
         throw error(403, 'Forbidden');
     }
-    
+
     throw error(401, 'Unauthorized');
 }
